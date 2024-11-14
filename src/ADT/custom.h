878ea013dd9512224719c7496f98c6a3a2239075
NazwanSM <19623200@mahasiswa.itb.ac.id>
2024-11-14 16:38:08 +0700

    ADT + config

---
+++ src/ADT/custom.h
@@ -0,0 +1,22 @@
+/* File: custom.h */
+
+#ifndef CUSTOM_H
+#define CUSTOM_H
+
+#define MAX_LEN 100
+
+#define integer int
+
+
+typedef struct {
+    char name[MAX_LEN];
+    char password[MAX_LEN];
+    integer money;
+} User;
+
+typedef struct {
+    char name[MAX_LEN];
+    integer price;
+} Barang;
+
+#endif
--- src/ADT/mesinkata.h
+++ src/ADT/mesinkata.h
@@ -13,7 +13,7 @@
 typedef struct
 {
    char TabWord[NMax]; /* container penyimpan kata, indeks yang dipakai [0..NMax-1] */
-    int Length;
+   int Length;
 } Word;
 
 /* State Mesin Kata */
@@ -28,22 +28,22 @@ I.S. : currentChar sembarang
 void STARTWORD();
 /* I.S. : currentChar sembarang
 F.S. : EndWord = true, dan currentChar = MARK;
-        atau EndWord = false, currentWord adalah kata yang sudah diakuisisi,
+      atau EndWord = false, currentWord adalah kata yang sudah diakuisisi,
           currentChar karakter pertama sesudah karakter terakhir kata */
 
 void ADVWORD();
 /* I.S. : currentChar adalah karakter pertama kata yang akan diakuisisi
 F.S. : currentWord adalah kata terakhir yang sudah diakuisisi,
-        currentChar adalah karakter pertama dari kata berikutnya, mungkin MARK
-        Jika currentChar = MARK, EndWord = true.
+      currentChar adalah karakter pertama dari kata berikutnya, mungkin MARK
+      Jika currentChar = MARK, EndWord = true.
    Proses : Akuisisi kata menggunakan procedure SalinWord */
 
 void CopyWord();
 /* Mengakuisisi kata, menyimpan dalam currentWord
 I.S. : currentChar adalah karakter pertama dari kata
 F.S. : currentWord berisi kata yang sudah diakuisisi;
-        currentChar = BLANK atau currentChar = MARK;
-        currentChar adalah karakter sesudah karakter terakhir yang diakuisisi.
+      currentChar = BLANK atau currentChar = MARK;
+      currentChar adalah karakter sesudah karakter terakhir yang diakuisisi.
           Jika panjang kata melebihi NMax, maka sisa kata "dipotong" */
 
 #endif
