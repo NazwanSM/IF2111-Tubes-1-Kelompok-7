cbcdcbaf80d340f3b4b246134fc60b9ff5b627bf
NazwanSM <19623200@mahasiswa.itb.ac.id>
2024-11-25 14:28:33 +0700

    fix2

--- src/logout.c
+++ src/logout.c
@@ -3,6 +3,6 @@
 
 
 void logoutUser(int *userIdx, List user) {
-    printf("%s telah logout dari sistem PURRMART.", user.A[*userIdx].name);
-    printf("Silakan REGISTER/LOGIN kembali untuk melanjutkan.");
+    printf("%s telah logout dari sistem PURRMART\n", user.A[*userIdx].name);
+    printf("Silakan REGISTER/LOGIN kembali untuk melanjutkan\n");
 }
