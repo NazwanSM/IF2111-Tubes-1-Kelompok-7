04baaf2f04adae954f799d2d068fcfab855ced87
NazwanSM <19623200@mahasiswa.itb.ac.id>
2024-12-18 18:47:02 +0700

    Merger dari Aben

--- src/storeList.c
+++ src/storeList.c
@@ -9,8 +9,9 @@ void storeList(ArrayDin barang, int nbarang){
     else {
         printf("\033[1;34mList barang yang ada di toko : \033[0m\n");
         for (int i = 0; i < nbarang; i++){
-            printf("- ");
-            printf("%s\n", barang.A[i].name);
+            printf(COLOR_BOLD_YELLOW"- "COLOR_OFF);
+            printf("%s - ", barang.A[i].name);
+            printf("Harga: "COLOR_BOLD_RED"%d\n"COLOR_OFF, barang.A[i].price);
         }
     }
 }
