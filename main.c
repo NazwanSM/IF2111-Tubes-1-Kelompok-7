743bf0bcf6c23736b6b4b3cc693dd049645e841a
NazwanSM <19623200@mahasiswa.itb.ac.id>
2024-12-18 18:27:09 +0700

    optimasi fungsi wishlistRemovei dan wishlistSwap

--- main.c
+++ main.c
@@ -316,47 +316,76 @@ int main() {
                 struct timespec req = {4, 0};
                 nanosleep(&req, NULL);
             }
-            else if (isKataSama(choice, "WISHLIST SWAP")) { //ini swap <i> <j>
-                int N, M;
-                printf("\nMasukkan nilai N dan M: ");
-                scanf("%d %d", &N, &M); // buat sementara
-                printf("\n\033[1;34m>> WISHLIST SWAP %d %d\033[0m\n\n", N, M);
+            else if (startsWith(choice, "WISHLIST SWAP")) { //ini swap <i> <j>
+                int N = 0;
+                int i = 14;
+                int M = 0;
+                int j = 0;
+
+                while (i < choice.Length && choice.TabWord[i] == ' ') {
+                    i++;
+                }
+
+                while (i < choice.Length && choice.TabWord[i] >= '0' && choice.TabWord[i] <= '9') {
+                    N = N * 10 + (choice.TabWord[i] - '0');
+                    i++;
+                }
+
+                while (i < choice.Length && choice.TabWord[i] == ' ') {
+                    i++;
+                }
+
+                while (i < choice.Length && choice.TabWord[i] >= '0' && choice.TabWord[i] <= '9') {
+                    M = M * 10 + (choice.TabWord[i] - '0');
+                    i++;
+                }
+                
+                printf(COLOR_BOLD_CYAN"\n>> WISHLIST SWAP %d %d\n\n"COLOR_OFF, N, M);
                 wishlistSwap(&user, userIdx, N, M);
 
-                struct timespec req = {4, 0};
+                struct timespec req = {2, 0};
                 nanosleep(&req, NULL);
             }
-            else if (isKataSama(choice, "WISHLIST REMOVE")) { //ini harusnya remove <i>
-                int N;
-                printf("\nMasukkan nilai N : ");
-                scanf("%d", &N);
+            else if (startsWith(choice, "WISHLIST REMOVE")) { //ini harusnya remove <i>
+                int N = 0;
+                int i = 16;
+
+                while (i < choice.Length && choice.TabWord[i] == ' ') {
+                    i++;
+                }
+
+                while (i < choice.Length && choice.TabWord[i] >= '0' && choice.TabWord[i] <= '9') {
+                    N = N * 10 + (choice.TabWord[i] - '0');
+                    i++;
+                }
+
                 if (1) { // ini mksdnya ngecek kalo angka ato bukan tp sementara gw giniin dulu
-                    printf("\n\033[1;34m>> WISHLIST REMOVE %d\033[0m\n\n", N);
+                    printf(COLOR_BOLD_CYAN"\n>> WISHLIST REMOVE %d\n\n"COLOR_OFF, N);
                     wishlistRemovei(&user, userIdx, N);
                 } else {
-                    printf("\n\033[1;34m>> WISHLIST REMOVE \033[0m\n\n");
+                    printf(COLOR_BOLD_CYAN"\n>> WISHLIST REMOVE \n\n"COLOR_OFF);
                     printf("Penghapusan barang WISHLIST gagal dilakukan, command tidak\n");
                 }
 
-                struct timespec req = {4, 0};
+                struct timespec req = {2, 0};
                 nanosleep(&req, NULL);
             }
             else if (isKataSama(choice, "WISHLIST CLEAR")) {
-                printf("\n\033[1;34m>> WISHLIST CLEAR \033[0m\n\n");
+                printf(COLOR_BOLD_CYAN"\n>> WISHLIST CLEAR \n\n"COLOR_OFF);
                 wishlistClear(&user, userIdx);
 
-                struct timespec req = {4, 0};
+                struct timespec req = {2, 0};
                 nanosleep(&req, NULL);
             }
             else if (isKataSama(choice, "WISHLIST SHOW")) {
-                printf("\n\033[1;34m>> WISHLIST SHOW \033[0m\n\n");
+                printf(COLOR_BOLD_CYAN"\n>> WISHLIST SHOW \n\n"COLOR_OFF);
                 wishlistShow(user, userIdx);
 
-                struct timespec req = {4, 0};
+                struct timespec req = {3, 0};
                 nanosleep(&req, NULL);
             }
             else if (isKataSama(choice, "LOGOUT")) {
-                printf(COLOR_BOLD_CYAN"\n>> LOGOUT\033[0m\n\n");
+                printf(COLOR_BOLD_CYAN"\n>> LOGOUT\n\n"COLOR_OFF);
                 logoutUser(&userIdx, user);
 
                 struct timespec req = {2, 0};
